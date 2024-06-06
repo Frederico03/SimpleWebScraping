03243b49dc841ebb7a5153f0b384b613cc2dc673
Frederico03 <fredericogarcez13@gmail.com>
2024-06-05 23:51:24 -0300

    Adicionar arquivos de projeto.

---
+++ WebScraping/Program.cs
@@ -0,0 +1,18 @@
+﻿using EasyAutomationFramework;
+using EasyAutomationFramework.Model;
+using WebScraping.Drive;
+
+var web = new WebScraper();
+var computers = web.GetData("https://webscraper.io/test-sites/e-commerce/allinone/computers");
+var laptops = web.GetData("https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops");
+var tablets = web.GetData("https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets");
+
+var myParams = new ParamsDataTable("Dados", @"C:\Users\Frederico Garcêz\Downloads", new List<DataTables>
+{
+    new DataTables("computers", computers),
+    new DataTables("laptops", laptops),
+    new DataTables("tablets", tablets)
+});
+
+Base.GenerateExcel(myParams);
+
